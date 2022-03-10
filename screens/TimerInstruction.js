71f1e18599e986e9df5531863580cf39df31bfa8
pierrejean <pj.sebastien@gmail.com>
2022-03-10 13:17:34 +0400

    add timer logik

--- screens/TimerInstruction.js
+++ screens/TimerInstruction.js
@@ -1,17 +1,20 @@
 import {
+    Alert,
     Dimensions,
-    ImageBackground,
     SafeAreaView,
     ScrollView,
     StyleSheet,
     Text,
+    TouchableHighlight,
     TouchableOpacity,
     View,
 } from 'react-native';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Colors from '../constants/Colors';
 import { Ionicons } from '@expo/vector-icons';
-import TimerComponent from '../components/timer/TimerComponent';
+import { Timer } from 'react-native-stopwatch-timer';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { Audio } from 'expo-av';
 
 const cookInstruction = {
     typeOfEgg: 'oeuf dur',
@@ -20,25 +23,56 @@ const cookInstruction = {
             id: 0,
             size: 'small',
             sizeIcon: 40,
-            time: 8,
+            time: 4800,
         },
         {
             id: 1,
             size: 'small',
             sizeIcon: 50,
-            time: 9,
+            time: 5400,
         },
         {
             id: 2,
             size: 'small',
             sizeIcon: 60,
-            time: 10,
+            time: 6000,
         },
     ],
 };
 
-const Timer = () => {
+const TimerInstruction = props => {
     const [cookTimer, setCookTimer] = useState(0);
+    const [isTimerStart, setIsTimerStart] = useState(false);
+    const [resetTimer, setResetTimer] = useState(true);
+    const [sound, setSound] = useState();
+
+    async function playSound() {
+        await Audio.setAudioModeAsync({
+            staysActiveInBackground: true,
+            interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
+            shouldDuckAndroid: true,
+            playThroughEarpieceAndroid: true,
+            allowsRecordingIOS: true,
+            interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+            playsInSilentModeIOS: true,
+        });
+        console.log('Loading Sound');
+        const { sound } = await Audio.Sound.createAsync(
+            require('../assets/temp/sf_coq_01.mp3'),
+            { isLooping: true, staysActiveInBackground: true },
+        );
+
+        setSound(sound);
+        await sound.playAsync();
+    }
+
+    useEffect(() => {
+        return sound
+            ? () => {
+                  sound.unloadAsync();
+              }
+            : undefined;
+    }, [sound]);
     return (
         <View style={styles.container}>
             <ScrollView showsVerticalScrollIndicator={false}>
@@ -60,7 +94,11 @@ const Timer = () => {
                                             key={size.id}
                                             activeOpacity={0.7}
                                             style={styles.eggButton}
-                                            onPress={() => setCookTimer(size.time)}
+                                            onPress={() =>
+                                                isTimerStart
+                                                    ? null
+                                                    : setCookTimer(size.time)
+                                            }
                                         >
                                             <Ionicons
                                                 name="egg"
@@ -90,9 +128,76 @@ const Timer = () => {
                                 </Text>
                             </View>
                         </View>
-                        <View style={{ backgroundColor: 'yellow', height: 100 }}>
-                            <TimerComponent />
-                            <Text>{cookTimer}</Text>
+                        <View>
+                            <View style={styles.timerContainer}>
+                                <TouchableHighlight
+                                    onPress={() => {
+                                        setIsTimerStart(!isTimerStart);
+                                        setResetTimer(false);
+                                    }}
+                                >
+                                    <Text style={styles.buttonText}>
+                                        {!isTimerStart ? (
+                                            <FontAwesome5
+                                                name="play"
+                                                size={30}
+                                                color={Colors.secondaryYellow}
+                                            />
+                                        ) : (
+                                            <FontAwesome5
+                                                name="pause"
+                                                size={30}
+                                                color={Colors.secondaryYellow}
+                                            />
+                                        )}
+                                    </Text>
+                                </TouchableHighlight>
+                                <Timer
+                                    totalDuration={cookTimer}
+                                    start={isTimerStart}
+                                    reset={resetTimer}
+                                    options={options}
+                                    handleFinish={() => {
+                                        Alert.alert(
+                                            'Fin de la cuisson !',
+                                            `Cliquez sur l'un des boutons ci dessous pour stopper l'alarme `,
+                                            [
+                                                {
+                                                    text: 'Retour',
+                                                    onPress: () => setSound(),
+                                                    style: 'cancel',
+                                                },
+                                                {
+                                                    text: 'Découvrir les recettes',
+                                                    onPress: () => {
+                                                        setSound(),
+                                                            props.navigation.navigate(
+                                                                'TabRecipes',
+                                                            );
+                                                        props;
+                                                    },
+                                                },
+                                            ],
+                                        );
+
+                                        setIsTimerStart(false);
+                                        setResetTimer(true);
+                                        playSound();
+                                    }}
+                                />
+                                <TouchableHighlight
+                                    onPress={() => {
+                                        setIsTimerStart(false);
+                                        setResetTimer(true);
+                                    }}
+                                >
+                                    <Ionicons
+                                        name="ios-reload-circle-sharp"
+                                        size={40}
+                                        color={Colors.secondaryYellow}
+                                    />
+                                </TouchableHighlight>
+                            </View>
                         </View>
                     </View>
                 </SafeAreaView>
@@ -101,7 +206,7 @@ const Timer = () => {
     );
 };
 
-export default Timer;
+export default TimerInstruction;
 
 const styles = StyleSheet.create({
     container: {
@@ -143,4 +248,27 @@ const styles = StyleSheet.create({
         fontFamily: 'Orkney-bold',
         fontSize: 18,
     },
+    timerContainer: {
+        flexDirection: 'row',
+        backgroundColor: Colors.secondaryGrayLight,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+        borderRadius: 25,
+    },
 });
+
+const options = {
+    container: {
+        backgroundColor: Colors.secondaryGrayLight,
+        padding: 5,
+        borderRadius: 5,
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 40,
+        color: Colors.secondaryYellow,
+        marginLeft: 7,
+    },
+};
